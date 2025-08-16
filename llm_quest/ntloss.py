db3c8b4669fb5f0b3a62920be47227b497220596
casinca <47400729+casinca@users.noreply.github.com>
2025-08-16 09:37:38 +0200

    - removed optional tiktoken tokenizer to match HF (byte conversion error) - simplified logic

--- llm_quest/ntloss.py
+++ llm_quest/ntloss.py
@@ -4,10 +4,10 @@ import torch
 class NumTokenLoss:
     def __init__(self, tokenizer, ntl_coeff=0.3, device=None, multi_digits=False):
         """
-        tokenizer (Tokenizer): tokenizer to use: tiktoken or transformers
+        tokenizer (Tokenizer): transformers tokenizer to use
         ntl_coeff (float): weight of the NumTokenLoss (default: 0.3)
         device (str): device to use for the NumTokenLoss
-        multi_digits (bool): whether to allow token to be a multiple digit number (default: False)
+        multi_digits (bool): whether to allow token to be a multiple digit number (ie "123")
         """
         self.ntl_coeff = ntl_coeff
         self.device = device
@@ -16,43 +16,23 @@ class NumTokenLoss:
         self.vocab_map = self.get_num_vocab_map(tokenizer)
 
     def get_num_vocab_map(self, tokenizer):
-        # --- retrieve vocab from the tokenizer ---
-        # transformers (default)
-        try:
-            vocab = tokenizer.get_vocab()
-
-        # tiktoken alt
-        except AttributeError:
-            try:
-                vocab = {
-                    tokenizer.decode_single_token_bytes(token_id).decode("utf-8", errors="strict"): token_id
-                    for token_id in range(tokenizer.n_vocab)
-                }
-
-            # if both fail
-            except Exception as e:
-                raise RuntimeError(f"Could not retrieve or construct vocabulary from the tokenizer. Error: {e}")
-
+        # retrieve vocab from the tokenizer
+        vocab = tokenizer.get_vocab()
         vocab_map = torch.full((len(vocab),), float("nan"))
 
-        # --- build vocab mapping from the vocab dictionary ---
+        # build vocab mapping from the vocab dictionary
         for string, token_id in vocab.items():
             stripped_token = string.strip()
 
-            if not stripped_token.isdigit() and stripped_token != "-1":
-                continue
-
             try:
                 token_value = float(stripped_token)
+                single_digit = -1 <= token_value <= 9 and len(stripped_token) == 1
 
-                if self.multi_digits:  # experimental: multi-digit tokens (ie "123")
+                if self.multi_digits or single_digit:
                     vocab_map[token_id] = token_value
-                else:  # Follow the paper: only -1 to 9 (single digit)
-                    if -1 <= token_value <= 9 and len(stripped_token) == 1:
-                        vocab_map[token_id] = token_value
 
             except ValueError:
-                print(f"!!! Parsing failed: Could not convert token {string} to float. Skipping...")
+                # print(f"!!! Parsing failed: Could not convert token {string} to float. Skipping...")
                 continue
 
         return vocab_map
