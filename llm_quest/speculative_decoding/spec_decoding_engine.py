f50bc1d4b138578284e5275e031d11f8f20867fe
casinca <47400729+casinca@users.noreply.github.com>
2025-09-04 17:18:16 +0200

    - added `_rejection_sampling` - added `speculative_sampling_greedy` - finished stochastic loop

--- llm_quest/speculative_decoding/spec_decoding_engine.py
+++ llm_quest/speculative_decoding/spec_decoding_engine.py
@@ -1,6 +1,6 @@
 import torch
 
-from llm_quest.gpt.generate import sampling
+from llm_quest.gpt.generate import _top_k_sampling, _top_p_sampling, sampling
 from llm_quest.gpt.gpt_attention import KVCache
 
 
@@ -31,12 +31,96 @@ def get_logprobs(logits, generated_tokens, temp=0.0):
     return gen_tokens_logprobs
 
 
-# NOTE: Passing full (PMF) distributions for acceptance might be weird as we don't need it for comparing sequentially
-# single tokens' probabilities p(x_i) and q(x_i). However, we still need them, in case of rejection, for the adjusted
-# distribution p'(x) = norm(max(0, p(x) - q(x))).
-# That's because we don't know beforehand which token will be rejected, hence the reason for keeping full distributions
+def _rejection_sampling(draft_logits, target_logits, top_k, top_p, temp):
+    """
+    Function to perform rejection sampling logic if a token is rejected
+
+    args:
+        draft_logits (torch.Tensor): The logits distribution from the draft model for the next token, shape (v)
+        target_logits (torch.Tensor): The logits distribution from the target model for the next token, shape (v)
+        top_k (int | None): limits sampling to top k most likely tokens. None to disable.
+        top_p (float | None): limits sampling to top p most likely tokens. Can be combined with top_k. range [0.0, 1.0].
+                                None to disable.
+        temp (float): The temperature for the distribution, needs to be > 0.0
+
+    returns:
+        torch.Tensor: The sampled token, shape (1)
+    """
+    assert temp > 0.0, "Temperature needs to be > 0.0, greedy decoding is handled separately"
+
+    if temp > 0.0:
+        target_logits = target_logits / temp
+        draft_logits = draft_logits / temp
+
+    target_probs = torch.softmax(target_logits, dim=-1)
+    draft_probs = torch.softmax(draft_logits, dim=-1)
+
+    zeroes = torch.zeros_like(target_probs)
+    adjusted_probs = torch.max(zeroes, target_probs - draft_probs)
+
+    if top_p:
+        adjusted_probs = _top_p_sampling(adjusted_probs, top_p, top_k)
+    elif top_k:
+        adjusted_probs = _top_k_sampling(adjusted_probs, top_k)
+
+    adjusted_probs /= adjusted_probs.sum(dim=-1, keepdim=True)  # renormalize to sum up to 1
+    next_token = torch.multinomial(adjusted_probs, num_samples=1)
+
+    return next_token
+
+
+def speculative_sampling_greedy(target_logits, generated_tokens):
+    """
+    Simplified route for greedy/deterministic case of speculative sampling, leveraging one hot encoding distributions we
+    don't need the draft model's distributions to compute acceptance/rejection.
+
+    args:
+    target_logits (torch.Tensor): The logit distributions from the target model for each next-token prediction in the
+    draft sequence, shape (b, draft_max_gen+1, v)
+    generated_tokens (torch.Tensor): The generated tokens, shape (b, draft_max_gen)
+
+    returns:
+        torch.Tensor: The accepted tokens + last token, shape (b, num_accepted + 1)
+    """
+    accepted_tokens = []
+    num_accepted = 0
+    num_drafted = generated_tokens.shape[1]
+
+    target_choices = torch.argmax(target_logits[:, :-1, :], dim=-1)
+
+    for i in range(num_drafted):
+        if target_choices[0, i] == generated_tokens[0, i]:
+            accepted_tokens.append(generated_tokens[0, i].item())
+            num_accepted += 1
+        else:
+            next_token = target_choices[0, i]
+            accepted_tokens.append(next_token.item())
+            break
+
+    if num_accepted == num_drafted:
+        next_token = sampling(target_logits[:, -1, :], temp=0.0)
+        accepted_tokens.append(next_token.item())
+
+    accepted_tokens = torch.tensor(accepted_tokens, device=generated_tokens.device).unsqueeze(0)
+
+    return accepted_tokens
+
+
+# NOTE: Passing distributions (PMF) for acceptance might be weird as we don't need it for comparing sequentially
+# single tokens' probabilities p(x_i) and q(x_i).
+# However, we still need them in case of:
+# - rejection, for the adjusted distribution p'(x) = norm(max(0, p(x) - q(x))).
+# - all accepted, for the bonus token sampled from the target model
+# And we don't know beforehand which token will be rejected, hence the reason for keeping distributions
 # from both models.
-def speculative_sampling(draft_logits, target_logits, generated_tokens, temp=0.0):
+def speculative_sampling(
+    draft_logits,
+    target_logits,
+    generated_tokens,
+    top_k,
+    top_p,
+    temp,
+):
     """
     Function to perform speculative sampling described in the paper algorithm 1, part 3,4 and 5:
     - Determine the number of accepted guesses n.
@@ -49,17 +133,50 @@ def speculative_sampling(draft_logits, target_logits, generated_tokens, temp=0.0
         target_logits (torch.Tensor): The logit distributions from the target model for each next-token prediction in the
         draft sequence, shape (b, draft_max_gen+1, v)
         generated_tokens (torch.Tensor): The generated tokens, shape (b, draft_max_gen)
-        temp (float): The temperature for the distribution
+        top_k (int | None): limits sampling to top k most likely tokens. None to disable.
+        top_p (float | None): limits sampling to top p most likely tokens. Can be combined with top_k. range [0.0, 1.0].
+                                None to disable.
+        temp (float): The temperature for the distribution, needs to be > 0.0
 
-    returns: TODO
+    returns:
+        torch.Tensor: The accepted tokens + last token, shape (b, num_accepted + 1)
     """
+    assert temp > 0.0, "Temperature needs to be > 0.0, greedy decoding is handled separately"
+
+    accepted_tokens = []
+    num_accepted = 0
+    num_drafted = generated_tokens.shape[1]
+
     draft_logprobs = get_logprobs(draft_logits, generated_tokens, temp)
     target_logprobs = get_logprobs(target_logits[:, :-1, :], generated_tokens, temp)
 
     ratios = torch.exp(target_logprobs - draft_logprobs)
 
-    for i in range(ratios.shape[1]):
-        pass
+    for i in range(num_drafted):
+        r = torch.rand(1).item()
+
+        # acceptance condition: r < p(x) / q(x), ie if p(x) >= q(x) or probabilistically if p(x) < q(x)
+        if r < ratios[0, i]:
+            accepted_tokens.append(generated_tokens[0, i].item())
+            num_accepted += 1
+        else:  # rejection: we sample the last token from the adjusted distribution
+            next_token = _rejection_sampling(
+                draft_logits[:, i, :],
+                target_logits[:, i, :],
+                top_k,
+                top_p,
+                temp,
+            )
+            accepted_tokens.append(next_token.item())
+            break
+
+    if num_accepted == num_drafted:  # all accepted, sample bonus token from target model x ~ p_γ+1(x)
+        next_token = sampling(target_logits[:, -1, :], top_k, top_p, temp)
+        accepted_tokens.append(next_token.item())
+
+    accepted_tokens = torch.tensor(accepted_tokens, device=generated_tokens.device).unsqueeze(0)
+
+    return accepted_tokens
 
 
 def speculative_generate(
@@ -82,19 +199,16 @@ def speculative_generate(
     # The target model isn't generating tokens, just being passed the prompt+generated tokens for its probabilities
     tokens_ids = []
 
-    kv_cache = KVCache(
-        num_layers=len(draft_model.trf_blocks),
-        max_seq_len=context_length,
-    )
+    kv_cache = KVCache(num_layers=len(draft_model.trf_blocks), max_seq_len=context_length)
 
     prompt = prompt.to(device)
     prompt_len = prompt.shape[1]
     trunc_prompt = prompt[:, -context_length:]
 
-    # --- Drafting tokens with the approximation/draft model ---
     with torch.inference_mode():
         draft_logits = draft_model(trunc_prompt, kv_cache=kv_cache)[:, -1, :]
 
+        # --- Drafting tokens with the approximation/draft model ---
         for _ in range(draft_max_gen):
             next_token = sampling(draft_logits, top_k, top_p, temp)
 
@@ -113,8 +227,16 @@ def speculative_generate(
         # Slicing as we are not interested in prompts logits:
         # - prompt_len - 1 to include the logits distribution of the 1st draft, from the last prompt token
         # - For the target model, we include the prediction from the last token, for the bonus token, shape (b, γ+1, v)
-        draft_logits = draft_model(full_tokens, kv_cache=None)[:, prompt_len - 1 : prompt_len + draft_max_gen - 1, :]
         target_logits = target_model(full_tokens, kv_cache=None)[:, prompt_len - 1 : prompt_len + draft_max_gen :, :]
         generated_tokens = full_tokens[:, prompt_len:]
 
-        speculative_sampling(draft_logits, target_logits, generated_tokens, temp)
+        if temp == 0.0:
+            accepted_tokens = speculative_sampling_greedy(target_logits, generated_tokens)
+        else:
+            # (inside else block to avoid unnecessary computation if temp == 0.0)
+            draft_logits = draft_model(full_tokens, kv_cache=None)[
+                :, prompt_len - 1 : prompt_len + draft_max_gen - 1, :
+            ]
+            accepted_tokens = speculative_sampling(draft_logits, target_logits, generated_tokens, top_k, top_p, temp)
+
+        return torch.cat([prompt, accepted_tokens], dim=-1)
--- llm_quest/speculative_decoding/spec_decoding_generate.py
+++ llm_quest/speculative_decoding/spec_decoding_generate.py
@@ -3,15 +3,20 @@ import torch
 
 import config
 from gpt_download import download_and_load_gpt2
+from llm_quest.gpt.generate import generate_loop_kv_cache
 from llm_quest.gpt.gpt_model import GPTModel
 from llm_quest.speculative_decoding.spec_decoding_engine import speculative_generate
-from llm_quest.utils import load_weights_into_gpt, text_to_ids
+from llm_quest.utils import ids_to_text, load_weights_into_gpt, text_to_ids
 
 # --- Hyperparameters ---
-max_gen = 40
-draft_max_gen = 10
+max_gen = 20
+draft_max_gen = 3
 target_context_length = 1024  # TODO switch to config
 draft_context_length = 768
+seed = 123
+temp = 1.4
+top_k = 25
+top_p = None
 
 prompt = "This is where it"
 
@@ -29,8 +34,6 @@ if __name__ == "__main__":
     target_model_config = config.config_creator("gpt_m")
     draft_model_config = config.config_creator("gpt_s")
 
-    torch.manual_seed(123)
-
     device = "cuda"
 
     target_model = GPTModel(target_model_config)
@@ -41,17 +44,61 @@ if __name__ == "__main__":
 
     target_model.to(device=device, dtype=torch.bfloat16).eval()
     draft_model.to(device=device, dtype=torch.bfloat16).eval()
-
-    speculative_generate(
+    torch.manual_seed(seed)
+    output = speculative_generate(
         target_model=target_model,
         draft_model=draft_model,
         prompt=text_to_ids(prompt, tokenizer=tokenizer),
         max_gen=max_gen,
         context_length=draft_context_length,  # TODO need both?
         draft_max_gen=draft_max_gen,
-        top_k=20,
-        top_p=1.0,
-        temp=1.0,
+        top_k=top_k,
+        top_p=top_p,
+        temp=temp,
         eos_id=50256,
         device=device,
     )
+    print("\nOutput from speculative decoding:")
+    print(output)
+    print("\n")
+    print(ids_to_text(output, tokenizer))
+
+    print("-------------")
+
+    #    torch.manual_seed(seed)
+    #    output2 = speculative_generate(
+    #        target_model=target_model,
+    #        draft_model=target_model,
+    #        prompt=text_to_ids(prompt, tokenizer=tokenizer),
+    #        max_gen=max_gen,
+    #        context_length=draft_context_length,
+    #        draft_max_gen=draft_max_gen,
+    #        top_k=top_k,
+    #        top_p=top_p,
+    #        temp=temp,
+    #        eos_id=50256,
+    #        device=device,
+    #    )
+    #    print("\nOutput from speculative decoding:")
+    #    print(output2)
+    #    print("\n")
+    #    print(ids_to_text(output2, tokenizer))
+    #
+    # print("-------------")
+
+    torch.manual_seed(seed)
+    output3 = generate_loop_kv_cache(
+        input_tensor=text_to_ids(prompt, tokenizer=tokenizer),
+        model=target_model,
+        max_gen=max_gen,
+        context_length=draft_context_length,
+        top_k=top_k,
+        top_p=top_p,
+        temp=temp,
+        eos_id=50256,
+        device=device,
+    )
+    print("\nOutput from target model:")
+    print(output3)
+    print("\n")
+    print(ids_to_text(output3, tokenizer))
