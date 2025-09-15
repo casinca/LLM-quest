fixed unoptimized redundant bool() conversion of the causal mask for all models using buffers

llm_quest/common/buffers.py
@@ -23,7 +23,7 @@ class GlobalBuffers:
         key = (ctx_len, rope_base, head_dim)
 
         if key not in GlobalBuffers._buffer:
-            mask = torch.triu(torch.ones(ctx_len, ctx_len), diagonal=1)
+            mask = torch.triu(torch.ones(ctx_len, ctx_len, dtype=torch.bool), diagonal=1)
             cos, sin = RoPE.compute_angles(rope_base, head_dim, ctx_len, smooth_scaling_cfg)
 
             GlobalBuffers._buffer[key] = (mask, cos, sin)

llm_quest/gpt_to_llama3/llama_attention.py
@@ -78,7 +78,7 @@ class GroupedQueryAttention(nn.Module):
         values = values.repeat_interleave(self.num_repeat, dim=1)
         att_scores = queries @ keys.mT  # shape (b, num_heads, seq_len, seq_len)
         # mask up to seq length/num of tokens
-        current_mask = mask.bool()[:seq_len, :seq_len]
+        current_mask = mask[:seq_len, :seq_len]
         # scaling by √(head_dim)
         scaled_att_scores = att_scores * self.att_scaling
         # masking in place and normalizing with softmax

llm_quest/llama3_to_deepseekv3/deepseek_attention.py
@@ -95,7 +95,7 @@ class MultiLatentAttention(nn.Module):
         # attention
         att_scores = queries @ keys.mT
         # mask up to seq length/num of tokens
-        current_mask = mask.bool()[:seq_len, :seq_len]
+        current_mask = mask[:seq_len, :seq_len]
         # scaling by √(head_dim + decoup_head_dim)
         scaled_att_scores = att_scores * self.att_scaling
         # masking in place and normalizing with softmax