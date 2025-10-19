3010fee0c7af2fcbde474786f005012302755334
casinca <47400729+casinca@users.noreply.github.com>
2025-10-19 16:08:05 +0200

    added `multimodal_loss`

--- llm_quest/multimodal/multimodal_engine.py
+++ llm_quest/multimodal/multimodal_engine.py
@@ -18,3 +18,24 @@ def get_embeddings(text_input, model):
     position_embeddings = model.pos_emb_dict(positions)
 
     return token_embeddings + position_embeddings
+
+
+def multimodal_loss(logits, labels, text_attention_mask, num_vision_tokens):
+    """
+    Classic NTP with a CE loss
+    We want to predict the text tokens given the image + text context
+    so image tokens/patches+padding are ignored, text_tokens are predicted
+
+    Args:
+        logits: (torch.tensor): shape (batch, seq_len, vocab_size)
+        labels: (torch.tensor): shape (batch, seq_len)
+        text_attention_mask: (torch.tensor): shape (batch, seq_len)
+        num_vision_tokens: (int): number of vision tokens in the input
+    """
+
+    shifted_logits = logits[:, num_vision_tokens - 1 : -1, :]  # the last vision token predicts the first text token
+    labels = labels.masked_fill(text_attention_mask == 0, -100)  # already aligned with shifted_logits
+
+    loss = F.cross_entropy(shifted_logits.flatten(0, 1), labels.flatten(), ignore_index=-100)
+
+    return loss
