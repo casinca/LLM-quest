6189dda83ea5d7e9f7f2f31524f186334791c893
casinca <47400729+casinca@users.noreply.github.com>
2026-01-09 16:15:16 +0100

    added `HyperConnectionPost`

--- llm_quest/common/hyper_connections/hyper_connections.py
+++ llm_quest/common/hyper_connections/hyper_connections.py
@@ -114,6 +114,10 @@ class HyperConnectionPre(nn.Module):
     This class is basically downprojecting the n expanded streams to a single stream, in order to pass into the
     transformer block, returning the output of H_pre @ x, described in eq 3 of the mHC paper.
 
+    Since the trf block has to be a single stream (to match traditional LLM implementation and not increase the time
+    complexity), H_pre weights serve to scale how much each of the n expanded streams contributes to this single merged
+    stream.
+
     Args:
         emb_dim (int): The dimension of the embeddings
         expansion_rate (int): The number of expanded streams, ("n" in the paper), can be seen as the width of the
@@ -147,7 +151,8 @@ class HyperConnectionPre(nn.Module):
         # scalar learnable gating factor, (alpha in the mHC paper, table 5 hparam init=0.01)
         self.factor = nn.Parameter(torch.tensor([0.01], device=device, dtype=dtype))
 
-        # dynamic mapping (theta_pre): downproject to a single stream
+        # dynamic mapping (theta_pre): downproject the emb dim to a scalar:
+        # This determines how much the trf block output contributes to each of the n expanded streams
         self.linear = nn.Linear(emb_dim, 1, bias=False, device=device, dtype=dtype)
         # Same init for all dynamic mapping weights as 0 (HC paper p.4 section 2.3)
         nn.init.zeros_(self.linear.weight)
@@ -188,7 +193,7 @@ class HyperConnectionPre(nn.Module):
 
     def forward(self, x):
         """
-        Aggregate/collapse the n expanded streams to a single stream via the pre mapping matrix H_pre
+        Aggregate/collapse the n expanded streams to a single stream with the pre mapping matrix H_pre weights
 
         This ends up being a weighted sum of the n streams using the dynamically generated H_pre weights.
 
@@ -205,6 +210,108 @@ class HyperConnectionPre(nn.Module):
         return x
 
 
+class HyperConnectionPost(nn.Module):
+    """
+    Classic Hyper-connection for the exit of the residual branch/post-transformer block, as depicted in:
+    - DeepSeek mHC: Manifold-Constrained Hyper-Connections paper (eq 3 and 5): https://arxiv.org/abs/2512.24880
+    - Hyper-Connections (HC) paper: https://arxiv.org/abs/2409.19606
+
+    This class is basically broadcasting the single stream output of the trf block back to the n expanded streams
+    weighted by their H_post weights. This is done in order to mix/add it with the other n streams from the main
+    branch (output from H_res @ x).
+    It returns the output of (H_post @ output trf block), described in eq 3 of the mHC paper.
+
+    Since we are operating with n streams and the trf block had to be a single stream (to match traditional LLM
+    implementation and not increase the time complexity), H_post weights serve to scale how much each broadcasted trf
+    block output contributes to each of the n expanded streams.
+    ex: stream[i] = H_post_weight[i] * trf_output
+
+    Args:
+        emb_dim (int): The dimension of the embeddings
+        expansion_rate (int): The number of expanded streams, ("n" in the paper), can be seen as the width of the
+                            residual stream
+        add_static_mapping (bool): Whether to add static mappings, ie adding biases (b_res in mHC the paper)
+        activation_cls (nn.Module): The activation function class, default is Tanh per the mHC paper
+        norm_cls (nn.Module): The normalization function class, default is RMSNorm per the mHC paper
+        device (torch.device):
+        dtype (torch.dtype):
+
+    Returns:
+        x: The n post-trf block scaled streams, after applying the post mapping H_post,
+        shape: (b, seq_len, exps_rate, emb_dim)
+    """
+
+    def __init__(
+        self,
+        emb_dim,
+        expansion_rate=4,
+        add_static_mapping=True,
+        activation_cls=nn.Tanh,
+        norm_cls=nn.RMSNorm,
+        device=None,
+        dtype=None,
+    ):
+        super().__init__()
+
+        self.norm = norm_cls(emb_dim, device=device, dtype=dtype)  # TODO recheck dtype for norm
+        self.activation = activation_cls()
+
+        # scalar learnable gating factor, (alpha in the mHC paper, table 5 hparam init=0.01)
+        self.factor = nn.Parameter(torch.tensor([0.01], device=device, dtype=dtype))
+
+        # dynamic mapping (theta_post), same logic as theta_pre:
+        # This determines how much the trf block output contributes to each of the n expanded streams
+        self.linear = nn.Linear(emb_dim, 1, bias=False, device=device, dtype=dtype)
+        # Same init for all dynamic mapping weights as 0 (HC paper p.4 section 2.3)
+        nn.init.zeros_(self.linear.weight)
+
+        # static mapping (biases b_post) initialization:
+        # uniform weights of 1s
+        self.bias = (
+            nn.Parameter(torch.ones(expansion_rate, device=device, dtype=dtype)) if add_static_mapping else None
+        )  # shape (exps_rate,)
+
+    def post_mapping_matrix(self, x):
+        """
+        Generates the post mapping/matrix H_post
+        This is our small "HyperNet" where we generate on the fly the weights H_post. A weight for each of the n
+        expanded streams that will scale the broadcasted trf block output contribution for each of the n
+        expanded streams.
+
+        Args:
+            x: The n untouched/identity/residual streams input, shape: (b, seq_len, exps_rate, emb_dim)
+
+        Returns:
+            The transposed post mapping/matrix H_post^T as a column vector, shape: (b, seq_len, exps_rate, 1)
+        """
+        x = self.norm(x)
+        # shape (b, seq_len, exps_rate, emb_dim) → (b, seq_len, exps_rate, 1) → (b, seq_len, exps_rate)
+        x = self.linear(x).squeeze(-1)  # apply dynamic mapping
+        x = self.activation(x) * self.factor
+
+        if self.bias is not None:  # add static mapping if enabled
+            x += self.bias
+
+        return x.unsqueeze(-1)  # unsqueeze serve as transpose here H_post^T making it a column vector
+
+    def forward(self, x, main_stream):
+        """
+        Broadcast the trf block output back to the n expanded streams and scaled with the post mapping matrix H_post
+        weights.
+
+        Args:
+            x: The single stream output of the trf block, shape: (b, seq_len, emb_dim)
+            main_stream: The n untouched/identity/residual streams input, shape: (b, seq_len, exps_rate, emb_dim)
+                        This is used to generate the post mapping matrix H_post.
+
+        Returns:
+            The n mixed streams, shape: (b, seq_len, exps_rate, emb_dim)
+        """
+
+        x = self.post_mapping_matrix(main_stream) @ x.unsqueeze(-2)
+        return x
+
+
 # quick inline test
 if __name__ == "__main__":
     import config
@@ -216,7 +323,10 @@ if __name__ == "__main__":
     test_x = torch.randn(1, 10, 4, 128, device=device, dtype=dtype)
     test_hc_res = HyperConnectionRes(emb_dim=128, expansion_rate=4, device=device, dtype=dtype)
     test_hc_pre = HyperConnectionPre(emb_dim=128, expansion_rate=4, device=device, dtype=dtype)
+    test_hc_post = HyperConnectionPost(emb_dim=128, expansion_rate=4, device=device, dtype=dtype)
     test_output_res = test_hc_res(test_x)
     test_output_pre = test_hc_pre(test_x)
+    test_output_post = test_hc_post(test_output_pre, test_x)
     print(test_output_res.shape)
     print(test_output_pre.shape)
+    print(test_output_post.shape)
