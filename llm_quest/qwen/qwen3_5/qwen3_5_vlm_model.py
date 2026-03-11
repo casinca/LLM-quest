1580608b12483ffd5972905ea0f644aeedef7954
casinca <47400729+casinca@users.noreply.github.com>
2026-03-11 12:54:21 +0100

    added `Qwen3_5VLM` forward

--- llm_quest/qwen/qwen3_5/qwen3_5_vlm_model.py
+++ llm_quest/qwen/qwen3_5/qwen3_5_vlm_model.py
@@ -1,3 +1,16 @@
+# Final Multimodal Qwen3.5 Vision-Language Model (VLM)
+#
+# This is the Main model that combines: vision encoder/ViT (Qwen3_5VisionModel) with the text model (Qwen3_5TextModel)
+# for multimodal generation.
+#
+# high level pipeline:
+# 1. Process token IDs (also contains image placeholders/special image tokens) through text encoder → text embeddings
+# 2. Process image_pixels through vision encoder → vision embeddings
+# 3. Replace image placeholder (special image token IDs) with vision embeddings in the input text embeddings
+# 4. Precompute 3D position IDs for MRoPE
+# 5. Run combined (text+vision) embeddings through text model with MRoPE
+
+
 import torch
 import torch.nn as nn
 
@@ -12,6 +25,10 @@ class Qwen3_5VLM(nn.Module):
     Composed of:
     - Qwen3_5VisionModel (vision encoder): preprocessed image pixels → vision embeddings
     - Qwen3_5TextModel (text model): combined embeddings in text embedding space (text + vision) → logits
+
+    The model supports two modes:
+    - Text-only: just pass input_ids (same as using Qwen3_5TextModel directly)
+    - Multimodal: pass input_ids + image_pixels
 
     Args:
         cfg (dict): Configuration dictionary shared by text and vision models:
@@ -28,12 +45,17 @@ class Qwen3_5VLM(nn.Module):
 
     def get_feeds_3d_shape(self, image_pixels):
         """
-        This retrieves the 3D shape (t, h, w) of each separated feed / number of independent visual inputs in the vision
-        prompt.
+        This retrieves the 3D shape (t, h, w), in term of patches, of each separated feed / number of independent visual
+        inputs in the vision prompt.
         Since multiple separated feeds (images or videos) are not supported from this, the number of feeds = 1
         So either 1 image or 1 video at a time
 
-        Not handling variable size, so H and W should be the same for all images
+        Not handling variable size, so H and W should be the same for all images.
+
+        Purpose: This is used at runtime to make MRoPE aware of the 3D image patch shapes in the input.
+        Very useful in case of variable sizes or mixed feeds (ex: 1x 4k image with an HD vid) but not the case here.
+
+        Hugging Face might compute this externally from the tokenizer or processor as `image_grid_thw`.
 
         Args:
             (both formats supported)
@@ -41,7 +63,7 @@ class Qwen3_5VLM(nn.Module):
             - 3D: (b, num_patches, features) → pre-extracted patches - (HuggingFace style, Qwen3_5VisionPatchEmbed)
 
         Returns:
-            feeds_3d_shape: (num_feeds, 3) shape of each separated feed / number of independent visual inputs
+            feeds_3d_shape: (num_feeds/1, 3) shape of each separated feed / number of independent visual inputs
 
         """
         n_height_patches = self.vision_model.n_height_patches
@@ -59,3 +81,56 @@ class Qwen3_5VLM(nn.Module):
             n_actual_frames = image_pixels.shape[1] // n_spatial_patches
 
         return torch.tensor([[n_actual_frames, n_height_patches, n_width_patches]])  # (1, 3) 3 for t, h, w
+
+    def forward(self, input_ids, image_pixels=None, feeds_3d_shape=None, attn_mask=None):
+        """
+        Forward pass for multimodal or text-only generation.
+
+        NOTE: When an input is multimodal, it's already "prepared" (at least the way HF does it and here). The text
+        sequence length doesn't increase because of vision input.
+        The text input is already expanded with image placeholder/special tokens that match the number of real vision
+        tokens/patches. Therefore we just replace with a mask the image placeholder/special tokens with the vision
+        tokens/patches.
+
+        Args:
+            input_ids: (b, seq_len)  token IDs (including image placeholders if multimodal)
+                    Image placeholders/special tokens are injected either manually or from HF processor.
+            image_pixels: (batch, in_channels, time, img_h, img_w)  preprocessed image pixels (None for text-only)
+            feeds_3d_shape: (num_feeds/1, 3)  3 for (t, h, w) (None for text-only)
+            attn_mask: (b, seq_len)  optional attention mask
+
+        Returns:
+            logits: (b, seq_len, vocab_size)
+        """
+        inputs_embs = self.language_model.emb_dict(input_ids)
+
+        # If multimodal, process images and replace image placeholders in the input embeddings by the vision embeddings
+        if image_pixels is not None:
+            # retrieves vision embeddings (projected to text dim)
+            vision_embeds = self.vision_model(image_pixels)  # (num_merged_patches, text_emb_dim)
+
+            image_mask = input_ids == self.image_token_id  # (b, seq_len)
+            # masked_scatter replaces True positions with values from vision_embeds into inputs_embs
+            # (b, seq_len, text_emb_dim) # no shape change, just replacing
+            inputs_embs = inputs_embs.masked_scatter(
+                image_mask.unsqueeze(-1).expand_as(inputs_embs), vision_embeds.to(inputs_embs.dtype)
+            )
+
+            # since multimodal, we also retrieve the complete 3D image shapes for reshaping correctly
+            #  the 3D position IDs
+            feeds_3d_shape = self.get_feeds_3d_shape(image_pixels)  # shape (T/n_actual_frame, 3)
+
+        # compute 3D position IDs for MRoPE
+        position_ids = self.compute_position_ids(input_ids, feeds_3d_shape)
+
+        # Forward pass through the text model with the complete input embeddings (text + vision)
+        logits = self.language_model(
+            inputs_embs=inputs_embs,
+            position_ids=position_ids,
+            attn_mask=attn_mask,
+        )
+
+        return logits
+
+
+# quick test
